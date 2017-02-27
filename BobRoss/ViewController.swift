82d21993f373ab2cc46dc6a99a8b2ee20ba918c2
ac <ac@Jaxxs-MBP.home>
2017-02-26 22:20:31 -0500

    Completed

--- BobRoss/ViewController.swift
+++ BobRoss/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var grassImageView: UIImageView!
+    @IBOutlet weak var sunImageView: UIImageView!
+    @IBOutlet weak var smallTreeImageView: UIImageView!
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var cloudImage: UIImageView!
+    @IBOutlet weak var kidsImage: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         // Do any additional setup after loading the view, typically from a nib.
+        grassImageView.image = #imageLiteral(resourceName: "Grass")
+        sunImageView.image = #imageLiteral(resourceName: "Sun")
+        smallTreeImageView.image = #imageLiteral(resourceName: "SmallTree")
+        treeImageView.image = #imageLiteral(resourceName: "Tree")
+        cloudImage.image = #imageLiteral(resourceName: "Clouds")
+        kidsImage.image = #imageLiteral(resourceName: "KidsPlaying")
+        
     }
 
     override func didReceiveMemoryWarning() {
